ca1060028823f9910c652bd65cc6e557337b9d14
Pranav <pranav.kumbhar02@gmail.com>
2025-04-28 21:55:55 +0530

    added todo and user db schemas

---
+++ src/models/TodoSceham.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const todoSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    isCompleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Todo", todoSchema);
