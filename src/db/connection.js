d2a7b531d867956d3b6794c0bb737a9bd4842bca
Pranav <pranav.kumbhar02@gmail.com>
2025-04-29 18:36:20 +0530

    fixed db connection

---
+++ src/db/connection.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const dbconnect = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI},`
+    );
+    console.log(
+      `MongoDB connected successfully! DB Host:
+      ${connectionInstance.connection.host}`
+    );
+  } catch (error) {
+    console.error("MongoDB connection Error:", error);
+    process.exit(1);
+  }
+};
+
+export default dbconnect;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
+import dotenv from "dotenv";
 import mongoose from "mongoose";
+import express from "express";
+import dbconnect from "./db/connection.js";
 
-const MONGOURI = process.env.MONGODB_URI;
+dotenv.config({ path: "./.env" });
+const app = express();
 
-(async () => {
-  try {
-    mongoose.connect(`${MONGOURI}`);
-    console.log("MongoDB connected successfully");
-  } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1);
-  }
-})();
-try {
-} catch (error) {}
+dbconnect();
+
+// const MONGOURI = process.env.MONGODB_URI;
+
+// (async () => {
+//   try {
+//     await mongoose.connect(`${MONGOURI}`);
+//     console.log("MongoDB connected successfully");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1);
+//   }
+// })();
+// try {
+// } catch (error) {}
